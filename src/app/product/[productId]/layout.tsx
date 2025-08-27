805236ccd5828cfa9f655d2f9aee7cf91e94dca2
Velkiv <kevinwalewangko078@gmail.com>
2025-08-27 19:51:43 +0800

    Error handling

--- src/app/product/[productId]/layout.tsx
+++ src/app/product/[productId]/layout.tsx
@@ -1,8 +1,18 @@
+
+function generateNumber(count: number) {
+    return Math.floor(Math.random() * count);
+}
+
 export default function ProductDetailsLayout({ 
     children,
 }: {
     children: React.ReactNode;
 }) {
+
+    const randomNumber = generateNumber(2);
+    if(randomNumber===1) {
+        throw new Error('Error loading occured for product');
+    }
     return <>
         {children}
         <h1>Featured Product</h1>
--- src/app/product/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/product/[productId]/reviews/[reviewId]/page.tsx
@@ -1,18 +1,18 @@
 import { redirect } from "next/navigation";
 
-function generateNumber(count: number) {
-    return Math.floor(Math.random() * count);
-}
+// function generateNumber(count: number) {
+//     return Math.floor(Math.random() * count);
+// }
 
 export default async function ProductReview( { 
     params, 
 }: { 
     params: Promise<{ productId: string, reviewId: string}>;
 }) {
-    const randomNumber = generateNumber(2);
-    if(randomNumber===1) {
-        throw new Error('Error loading occured');
-    }
+    // const randomNumber = generateNumber(2);
+    // if(randomNumber===1) {
+    //     throw new Error('Error loading occured');
+    // }
     const { productId, reviewId} = await params;
     if(parseInt(reviewId) > 1000) {
         // notFound();
